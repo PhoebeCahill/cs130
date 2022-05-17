8792c8a1bf986d2458a330cc419a4b3abb57010c
Phoebe Cahill <phoebecahill2024@u.northwestern.edu>
2022-05-16 20:31:03 -0500

    commit

--- hws/hw04/your_task/js/scripts.js
+++ hws/hw04/your_task/js/scripts.js
@@ -18,8 +18,36 @@ const search = (ev) => {
     }
 }
 
-const getTermHTML = (track) => {
+const getTracksHTML = (track) => {
+    //take in tracks array, so need to cycle through for each track to render image
+    
+    console.log(track);
+        //if track image does not exist, put this blank image instead
+    if (!track.image_url) {
+        track.image_url = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+    }
 
+    //EXTRA CREDIT! --> why isn't this working??
+    // if (!track.preview_url) {
+    //     //no audio preview availible if no preview URL --> why isn't this working?
+    //     document.querySelector("i.play-track") = null;
+    // } 
+    
+    //else {}
+        //added onclick event handler here to tracks 
+        return `<button class="track-item preview" aria-label="Preview ${track.name}"
+        onclick="handleTrackClick(event);"
+        data-preview-track="${track.preview_url}">
+        <img src="${track.album.image_url}" alt = "Photo of ${track.name} Album">
+        <i class="fas play-track fa-play" aria-hidden="true"></i>
+        <div class="label">
+            <h2>${track.name}</h2>
+            <p>
+            ${track.artist.name}
+            </p>
+        </div>
+    </button>`
+    
 };
 
 const getTracks = (term) => {
@@ -28,27 +56,79 @@ const getTracks = (term) => {
         "${term}" and load them into the #tracks section 
         of the DOM...`);
 
+    //clear the tracks container before fetch
+    document.querySelector("#tracks").innerHTML ="";
+
      //query Spotify via fetch and using baseURL from above and adding search term
     //could also have used template: `${baseURL}?type=artist&q=${term}`
+    //this code fetches tracks based on a search term and prints them to the console:
     fetch (baseURL + "?type=track&q=" + term)
     //takes response and converts into json object which we can work with
      .then(response =>  response.json())
-     .then ((data) => {
-         console.log(data);
-         //make sure at least one response 
-         if (data.length>0) {
+     .then (tracks => {
+
+        //data holds list of 20 tracks
+         console.log(tracks);
+
+         let firstTracks = [];
+
+         //can limit number of tracks returned here by looping as done here
+         //or can put limit into fetch URL
+         if (tracks.length>=5) {
              //pick the first artist that you go across (similar to other parts of 
              //homework where you might just need to use the loop)
-             const firstTracks = [data[0],data[1], data[2], data[3], data[4]];
+             firstTracks = [tracks[0],tracks[1], tracks[2], tracks[3], tracks[4]];
              //set innerHTML to the getArtistHTML function output with putting in
              //firstArtist as input
-             //document.querySelector("#term").innerHTML = getTermHTML(firstTracks);
-
-
-         } else {
-            document.querySelector("#artist").innerHTML ="No tracks found.";
+             console.log(firstTracks);
+             //or could have used classic for loop with i<5 and tracks[i]
+             for (const track of firstTracks) {
+                 //need to do += so adds on top of each other
+                document.querySelector("#tracks").innerHTML += getTracksHTML(track);
+             };
+         } else if (tracks.length>0 && tracks.length<=4) {
+             //fill firstTracks array with data information 
+             let i =0;
+             //could have also likely simplified this by just using a classic for loop
+             for (const track of tracks) {
+                firstTracks[i] = tracks[i];
+                i++;
+             }
+             for (const track of firstTracks) {
+                //need to do += so adds on top of each other
+               document.querySelector("#tracks").innerHTML += getTracksHTML(track);
+            };
+         }
+         //this code handles the case where no tracks are found
+         else if (tracks.length===0) {
+            document.querySelector("#tracks").innerHTML = `<p>No tracks found for ${term}.</p>`;
          }
      });
+};
+
+const getAlbumsHTML = (album) => {
+    //take in tracks array, so need to cycle through for each track to render image
+    //why is this not cyclying through all the albums?? why only once? 
+    
+         //if track image does not exist, put this blank image instead
+        if (!album.image_url) {
+            album.image_url = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+        }
+
+        console.log("album 1");
+
+        return `<section class="album-card" id="${album.id}" onclick="handleAlbumClick(event);">
+        <div>
+            <img src="${album.image_url}" alt = "Photo of ${album.name} Album">
+            <h2>${album.name}</h2>
+            <div class="footer">
+                <a href="${album.spotify_url}" target="_blank">
+                    view on spotify
+                </a>
+            </div>
+        </div>
+    </section>`
+   
 };
 
 const getAlbums = (term) => {
@@ -56,6 +136,30 @@ const getAlbums = (term) => {
         get albums from spotify based on the search term
         "${term}" and load them into the #albums section 
         of the DOM...`);
+    
+    document.querySelector("#albums").innerHTML = "";
+
+    fetch (baseURL + "?type=album&q=" + term)
+    //takes response and converts into json object which we can work with
+     .then(response =>  response.json())
+     .then ((data) => {
+         console.log(data);
+
+         //make sure at least one response
+         if (data.length>0) {
+             //fill album array with data information 
+             let albums = [];
+             //how to get this to not cycle for 
+             for (let i = 0; i < data.length; i++) {
+                albums[i] = data[i];
+                document.querySelector("#albums").innerHTML += getAlbumsHTML(albums[i]);
+             };
+             
+         }
+         else {
+            document.querySelector("#albums").innerHTML =`<p>No albums found for ${term}.</p>`;
+         }
+     });
 };
 
 //getArtist helper function to insert first artist into the template
@@ -66,9 +170,9 @@ const getArtistHTML = (artist) => {
     }
 
     //return image card, but make it based on what the user puts in the search
-    return `<section class="artist-card" id="${artist.id}">
+    return `<section class="artist-card" id="${artist.id}" onclick="handleArtistClick(event);">
     <div>
-        <img src="${artist.image_url}">
+        <img src="${artist.image_url}" alt = "Photo of ${artist.name} Album">
         <h2>${artist.name}</h2>
         <div class="footer">
             <a href="${artist.spotify_url}" target="_blank">
@@ -102,17 +206,49 @@ const getArtist = (term) => {
              //firstArtist as input
              document.querySelector("#artist").innerHTML = getArtistHTML(firstArtist);
 
-
          } else {
-            document.querySelector("#artist").innerHTML ="No artist found.";
+            document.querySelector("#artist").innerHTML =`<p>No artists found for ${term}.</p>`;
          }
      });
 };
 
+
+//why aren't these working??
 const handleTrackClick = (ev) => {
     const previewUrl = ev.currentTarget.getAttribute('data-preview-track');
     console.log(previewUrl);
+
+    audioPlayer.setAudioFile(previewUrl);
+
+    audioPlayer.play();
+
+    document.querySelector("footer .track-item").innerHTML = ev.currentTarget.innerHTML;
+
 }
+
+//EXTRA CREDIT --> why are these not working??
+const handleArtistClick = (ev) => {
+    const artistName = ev.currentTarget.getAttribute('artist.name');
+
+    //How to change the URL accordingly?
+    baseURL = `https://api.spotify.com/v1/artists/${artistName}/top-tracks`;
+
+    getTracks(artistName);
+
+
+    console.log("here they are " + getTracks(artistName));
+}
+
+const handleAlbumClick = (ev) => {
+
+    let
+
+    const albumName = ev.currentTarget.getAttribute('album.name');
+
+    getTracks(albumName);
+
+}
+
 
 document.querySelector('#search').onkeyup = (ev) => {
     // Number 13 is the "Enter" key on the keyboard, so onkeyup means when you 
